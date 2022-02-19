1fa05a20a96a1bc1409d507614e69f2f89e36c28
kramstyles <kramstyles@outlook.com>
2022-02-19 21:35:22 +0100

    added classes object attr

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 class BigObject:
     pass
 
+
 class PlayerCharacter:
-    def __init__(self, name):
+    techie = True
+
+    def __init__(self, name, age=18):
         self.name = name
+        self.age = age
 
     def run(self):
         print("Player:", self.name, "is running!")
 
+
 obj1 = BigObject()
 jnr = PlayerCharacter('Junior')
-
+dan = PlayerCharacter('Daniel Edem', 24)
 
 if __name__ == '__main__':
-    jnr.run()
+    print(dan.techie)
